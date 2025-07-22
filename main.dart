cca0390d08f01ea87fb56c818da2e09e90dc38ee
Akshay-770 <gogarkanabhai@gmail.com>
2025-07-22 22:19:06 +0530

    22/07

--- main.dart
+++ main.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_nv_sir/1507.flutter/basiccomand.dart'show containerExamle;
 
-import 'package:flutter_nv_sir/1507.flutter/columnexample.dart';
-
+// import 'package:flutter_nv_sir/1507flutter/basiccompo.dart';
+// import 'package:flutter_nv_sir/1507flutter/columnexample.dart';
+// import 'package:flutter_nv_sir/2207flutter/calculatorlayout.dart';
+import 'package:flutter_nv_sir/2207flutter/stopwatch.dart';
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-
-      home: SafeArea(
-        child: ColumnExample()
-      ),
+      home: Stopwatch(),
     );
-    
   }
 }
